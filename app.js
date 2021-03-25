21760014c999ff266ee7c08a22c9012676cb7902
Sherry <sherry.c.liao@gmail.com>
2021-03-26 01:02:46 +0800

    feat: add login.hbs && register.hbs

--- app.js
+++ app.js
@@ -14,10 +14,33 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('running')
 })
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Express is now listening on localhost: ${PORT}`)
