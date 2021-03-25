1a07a6353a6318e778299f32798ace3d2acd845f
Sherry <sherry.c.liao@gmail.com>
2021-03-26 00:10:31 +0800

    project init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+
+// const session = require('express-session')
+// const passport = require('passport')
+
+const app = express()
+const PORT = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('running')
+})
+
+app.listen(PORT, () => {
+  console.log(`Express is now listening on localhost: ${PORT}`)
+})
